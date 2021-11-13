88edb8d9f7e5aff7d9c135c9c12690b01e1af726
Kyle Andrews <kyleandrews@mail.fresnostate.edu>
2021-11-12 18:52:40 -0800

    FINASH HAM

--- project1.cpp
+++ project1.cpp
@@ -3,20 +3,20 @@
 #include <pthread.h>
 #include <unistd.h>
 #include "BBQ.h"
+#define N 2 //10 treads "dont tread on me"
 
 using namespace std;
 
-static BBQ *qq = new BBQ(10);
+BBQ *qq = new BBQ(10);
 
 void *add_function(void *q)
 {
-    int id;
-    int i = 0;
-    id = 12;
+   int i = 0;
 
     while (1)
     {
-        qq->insert(id);
+        qq->insert(i);
+        cout <<"ADDING SHIT\n";
         i++;
         sleep(1);
     }
@@ -26,14 +26,13 @@ void *remove_function(void *q)
 {
     int id;
     int i = 0;
-    id = 12;
 
     while (1)
     {
-        qq->remove();
-        printf("%d : %d\n", id, i);
+        id = qq->remove();
+        printf("REMOVING %d : (iter) %d (Queue Size) %d\n", id, i, qq->size());
         i++;
-        sleep(1);
+        sleep(10);
     }
 }
 
@@ -41,29 +40,35 @@ int main(int, char **)
 {
     // Create producer and consumer threads
 
-    pthread_t p_thread[2];
-    int thr_id0,thr_id1;
+    pthread_t p_thread[N];
+    int thr_add, thr_rem;
     int status;
     int a = 1;
     int b = 2;
 
-    thr_id0 = pthread_create(&p_thread[0], NULL, add_function, &a);
-    if (thr_id0 < 0)
+for(int i=0;i<N/2+1;i++) {
+    thr_add = pthread_create(&p_thread[i], NULL, add_function, &a);
+    if (thr_add < 0)
     {
-        perror("thread create error : ");
+        perror("thread create error on add_function: ");
+        cout << i << endl;
         exit(0);
     }
+}
 
-    thr_id1 = pthread_create(&p_thread[1], NULL, remove_function, &b);
-    if (thr_id1 < 0)
+for(int i=N/2;i<N;i++) {
+    thr_rem = pthread_create(&p_thread[i], NULL, remove_function, &b);
+    if (thr_rem < 0)
     {
-        perror("thread create error : ");
+        perror("thread create error on remove_function: ");
+        cout << i << endl;
         exit(0);
     }
-    pthread_join(p_thread[0], NULL);
-    pthread_join(p_thread[1],NULL);
+}
 
-    cout << qq->size() << endl;
+// Join threads plz
+for(int i=0;i<N;i++)
+    pthread_join(p_thread[i], NULL);
 
     return 0;
 }
